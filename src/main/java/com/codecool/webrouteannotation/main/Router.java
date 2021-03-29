5e812726251e92fb85f46ef244d75c9c05eb3204
Máté Groholy <mate.groholy@gmail.com>
2021-03-29 17:40:40 +0200

    feature: add employees get and employees post handlers. Add invalid route handler to Router.

--- src/main/java/com/codecool/webrouteannotation/main/Router.java
+++ src/main/java/com/codecool/webrouteannotation/main/Router.java
@@ -1,22 +1,43 @@
 package com.codecool.webrouteannotation.main;
 
 import com.codecool.webrouteannotation.main.annotations.WebRoute;
+import com.codecool.webrouteannotation.main.data.DummyData;
 import com.codecool.webrouteannotation.main.handlers.ResponseHandler;
 import com.sun.net.httpserver.HttpExchange;
 
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.nio.charset.StandardCharsets;
+import java.util.stream.Collectors;
 
 public class Router {
 
-    @WebRoute(path="/first")
-    public static void first(HttpExchange exchange) throws IOException {
-        String firstResponse = "first";
-        ResponseHandler.sendResponse(exchange, firstResponse);
+    private DummyData dummyData;
+
+    public Router(DummyData dummyData) {
+        this.dummyData = dummyData;
     }
 
-    @WebRoute(path="/second")
-    public static void second(HttpExchange exchange) throws IOException {
-        String secondResponse = "second";
-        ResponseHandler.sendResponse(exchange, secondResponse);
+    @WebRoute(path="/employees", method=HttpMethod.GET)
+    public void getEmployees(HttpExchange exchange) throws IOException {
+        ResponseHandler.sendResponse(exchange, dummyData.getEmployees().toString());
+    }
+
+    @WebRoute(path="/employees", method= HttpMethod.POST)
+    public void addEmployee(HttpExchange exchange) throws IOException {
+        InputStream inputStream = exchange.getRequestBody();
+        String name = new BufferedReader(
+                new InputStreamReader(inputStream, StandardCharsets.UTF_8)).lines()
+                .collect(Collectors.joining("\n"));
+
+        dummyData.addEmployee(name);
+        ResponseHandler.sendResponse(exchange, dummyData.getEmployees().toString());
+    }
+
+    public void invalidPath(HttpExchange httpExchange, String endpoint) throws IOException {
+        String response = "Invalid path. Nothing found on " + endpoint + " endpoint.";
+        ResponseHandler.sendResponse(httpExchange, response);
     }
 }
