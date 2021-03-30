a69418643e55c2520560c8f12d9285afd08d64bc
Máté Groholy <mate.groholy@gmail.com>
2021-03-30 11:47:55 +0200

    feature: create Employee class.

---
+++ src/main/java/com/codecool/webrouteannotation/main/data/Employee.java
@@ -0,0 +1,26 @@
+package com.codecool.webrouteannotation.main.data;
+
+import java.io.Serializable;
+
+public class Employee implements Serializable {
+
+    private int id;
+    private String name;
+
+    public Employee(int id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
